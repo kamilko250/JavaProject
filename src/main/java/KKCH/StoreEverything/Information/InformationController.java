3a8b6d58338ea32b5888464ed9a2bf06fb4612a7
Łukasz <43967017+magik9907@users.noreply.github.com>
2022-06-05 20:14:47 +0200

    bug fix, get information list from now on default

--- src/main/java/KKCH/StoreEverything/Information/InformationController.java
+++ src/main/java/KKCH/StoreEverything/Information/InformationController.java
@@ -14,14 +14,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.net.Authenticator;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/information")
@@ -32,23 +42,23 @@ public class InformationController {
     private AppUserService userService;
 
     @GetMapping("/add")
-    public String showInformationForm( Model model) {
+    public String showInformationForm (Model model) {
 
         model.addAttribute("information", new InformationDto(new CategoryDto()));
         List<CategoryDto> categoryDtoList = categoryService.getAll()
                 .stream()
-                .map(category->modelMapper.map(category,CategoryDto.class))
+                .map(category -> modelMapper.map(category, CategoryDto.class))
                 .toList();
         model.addAttribute("categories", categoryDtoList);
         return "add-information";
     }
 
     @GetMapping("/{id}/edit")
-    public String showEditForm (@PathVariable("id") Long id ,Model  model) {
+    public String showEditForm (@PathVariable("id") Long id, Model model) {
         Optional<InformationOrm> information = informationService.getById(id);
-        if(!information.isPresent())
+        if (!information.isPresent())
             return "redirect:/";
-        model.addAttribute("information", modelMapper.map(information.get(),InformationDto.class));
+        model.addAttribute("information", modelMapper.map(information.get(), InformationDto.class));
         List<CategoryDto> categoryDtoList = categoryService.getAll()
                 .stream()
                 .map(category -> modelMapper.map(category, CategoryDto.class))
@@ -59,18 +69,22 @@ public class InformationController {
 
     @Transactional
     @PostMapping("/add")
-    public String addInformation (@Valid @ModelAttribute("information")InformationDto information, BindingResult result, Model model, Authentication auth) {
+    public String addInformation (@Valid @ModelAttribute("information") InformationDto information,
+                                  BindingResult result,
+                                  Model model,
+                                  Authentication auth
+    ) {
         if (result.hasErrors()) {
             return "add-information";
         }
-        if(information.getId() == null)
-          information.setAddDate(LocalDate.now());
+        if (information.getId() == null)
+            information.setAddDate(LocalDate.now());
 
-        InformationOrm informationOrm = modelMapper.map(information,InformationOrm.class);
+        InformationOrm informationOrm = modelMapper.map(information, InformationOrm.class);
 
         CustomUser user = null;
         AppUser creator = null;
-        if(auth != null) {
+        if (auth != null) {
             user = (CustomUser) auth.getPrincipal();
             creator = this.userService.get(user.getId());
 
@@ -84,9 +98,15 @@ public class InformationController {
     }
 
     @GetMapping("/list")
-    public String getAllInformations(@RequestParam(name = "startDate",required = false) String startDate, @RequestParam(name = "category", required = false) String selectedCategory,
-                                     @RequestParam(name="endDate", required = false) String endDate, @RequestParam(name="sort", required = false) String sort, Model  model, HttpServletResponse response,
-                                     @CookieValue(value = "sort", defaultValue = "date_asc") String sortCookie, Authentication auth){
+    public String getAllInformations (@RequestParam(name = "startDate", required = false) String startDate,
+                                      @RequestParam(name = "category", required = false) String selectedCategory,
+                                      @RequestParam(name = "endDate", required = false) String endDate,
+                                      @RequestParam(name = "sort", required = false) String sort,
+                                      Model model,
+                                      HttpServletResponse response,
+                                      @CookieValue(value = "sort", defaultValue = "date_asc") String sortCookie,
+                                      Authentication auth
+    ) {
         LocalDate StartDate = startDate == null || startDate.isEmpty()
                 ? LocalDate.now()
                 : LocalDate.parse(startDate);
@@ -105,41 +125,55 @@ public class InformationController {
         String finalSelectedCategory = selectedCategory;
         List<InformationOrm> informationOrms = informationService.getAll(allowed);
         CategoryOrm categoryOrm = null;
-            if(selectedCategory != null && selectedCategory != "") {
-            Optional<CategoryOrm> categoryOrm1 =  categoryService.getAll().stream().filter(x-> x.getName().equals(finalSelectedCategory)).findFirst();
-            if(categoryOrm1.isPresent())
+        if (selectedCategory != null && selectedCategory != "") {
+            Optional<CategoryOrm> categoryOrm1 = categoryService.getAll()
+                    .stream()
+                    .filter(x -> x.getName()
+                            .equals(finalSelectedCategory))
+                    .findFirst();
+            if (categoryOrm1.isPresent())
                 categoryOrm = categoryOrm1.get();
         }
 
-        List<InformationOrm> filteredInformationOrms = new ArrayList<>() ;
+        List<InformationOrm> filteredInformationOrms = new ArrayList<>();
 
-        for(var inf : informationOrms)
-        {
-            if(categoryOrm != null && !inf.getCategory()
+        for (var inf : informationOrms) {
+            if (categoryOrm != null && !inf.getCategory()
                     .getId()
-                    .equals(categoryOrm.getId()))
+                    .equals(categoryOrm.getId())) // sortowanie po kategorii
                 continue;
-            if(startDate != null && !startDate.equals("") && inf.getAddDate().isBefore(StartDate))
+            if (startDate == null && endDate == null && inf.getAddDate()
+                    .isBefore(StartDate)) // sortowanie po aktualnej dacie
                 continue;
-            if(endDate != null && !endDate.equals("") && inf.getAddDate().isAfter(EndDate))
+            ;
+            if (startDate != null && !startDate.equals("") && inf.getAddDate()
+                    .isBefore(StartDate)) // sortowanie po dacie startowej
                 continue;
-            if(!(inf.isUserAllowed(allowed) || inf.getAppUser().getId().equals(Objects.requireNonNull(user)
-                                                                                      .getId())))
+            if (endDate != null && !endDate.equals("") && inf.getAddDate()
+                    .isAfter(EndDate))// sortowanie po dacie końcowej
+                continue;
+            if (!(inf.isUserAllowed(allowed) || inf.getAppUser()
+                    .getId()
+                    .equals(Objects.requireNonNull(user)
+                                    .getId())))
                 continue;
 
             filteredInformationOrms.add(inf);
         }
 
-        List<InformationDto> informationDto = modelMapper.map(filteredInformationOrms, new TypeToken<List<InformationDto>>() {}.getType());
+        List<InformationDto> informationDto = modelMapper.map(filteredInformationOrms,
+                                                              new TypeToken<List<InformationDto>>() {
+                                                              }.getType()
+        );
 
-        if(sort != null || sortCookie != null){
-            if(sort == null && sortCookie != null)
+        if (sort != null || sortCookie != null) {
+            if (sort == null && sortCookie != null)
                 sort = sortCookie;
 
             Cookie cookie = new Cookie("sort", sort);
             response.addCookie(cookie);
 
-            switch(sort){
+            switch (sort) {
                 case "date_asc":
                     Collections.sort(filteredInformationOrms, Comparator.comparing(obj -> obj.getAddDate()));
                     break;
@@ -148,10 +182,12 @@ public class InformationController {
                     Collections.reverse(filteredInformationOrms);
                     break;
                 case "category_asc":
-                    Collections.sort(filteredInformationOrms, Comparator.comparing(obj -> obj.getCategory().getName()));
+                    Collections.sort(filteredInformationOrms, Comparator.comparing(obj -> obj.getCategory()
+                            .getName()));
                     break;
                 case "category_desc":
-                    Collections.sort(filteredInformationOrms, Comparator.comparing(obj -> obj.getCategory().getName()));
+                    Collections.sort(filteredInformationOrms, Comparator.comparing(obj -> obj.getCategory()
+                            .getName()));
                     Collections.reverse(filteredInformationOrms);
                     break;
                 case "alphabetic_asc":
@@ -179,24 +215,24 @@ public class InformationController {
 
         model.addAttribute("categories", categoryDtoList);
         model.addAttribute("category", selectedCategory);
-        model.addAttribute("end_date", endDate );
+        model.addAttribute("end_date", endDate);
         model.addAttribute("start_date", startDate);
-        model.addAttribute("sort", sort );
+        model.addAttribute("sort", sort);
         return "get-all";
     }
 
     @GetMapping("/{id}")
-    public String getInformationById(@PathVariable("id") Long id, Model model) {
+    public String getInformationById (@PathVariable("id") Long id, Model model) {
         model.addAttribute("users", userService.getAll());
         Optional<InformationOrm> informationOrm = informationService.getById(id);
-        if(informationOrm.isPresent()) {
+        if (informationOrm.isPresent()) {
             model.addAttribute("information", modelMapper.map(informationOrm.get(), InformationDto.class));
         }
         return "get_by_id";
     }
 
     @PostMapping("/{id}/delete")
-    public String deleteInformationById(@PathVariable("id") Long id){
+    public String deleteInformationById (@PathVariable("id") Long id) {
         informationService.DeleteById(id);
 
         return "redirect:/information/list";
@@ -218,6 +254,8 @@ public class InformationController {
     }
 
     @Autowired
-    public void setUserService (AppUserService appUserService) { this.userService = appUserService; }
+    public void setUserService (AppUserService appUserService) {
+        this.userService = appUserService;
+    }
 
 }
