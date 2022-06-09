b5471e4f37570223e39553695e83215588920323
Cezary Hodun <cezaryh1106@gmail.com>
2022-06-09 20:03:13 +0200

    user role test

---
+++ src/test/java/KKCH/StoreEverything/AppUser/AppUserTests.java
@@ -0,0 +1,55 @@
+package KKCH.StoreEverything.AppUser;
+
+import KKCH.StoreEverything.Category.CategoryOrm;
+import KKCH.StoreEverything.Information.InformationOrm;
+import KKCH.StoreEverything.Role.UserRole;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootTest
+public class AppUserTests {
+    static UserRole admin = null;
+    static UserRole basicUser = null;
+
+    @BeforeAll
+    public static void CreateRoles(){
+        admin = new UserRole("admin_role", "Admin");
+        basicUser = new UserRole("user_role", "User");
+    }
+
+    @AfterAll
+    public static void Cleanup(){
+        admin = null;
+        basicUser = null;
+    }
+
+    @Test
+    public void AddRoleToUser(){
+        AppUser user = new AppUser();
+        Set<UserRole> roles = new HashSet<>();
+        roles.add(admin);
+        roles.add(basicUser);
+
+        user.setRoles(roles);
+
+        assert user.getRoles().size() == 2;
+    }
+
+    @Test
+    public void RemoveRoleFromUser(){
+        AppUser user = new AppUser();
+        Set<UserRole> roles = new HashSet<>();
+        roles.add(admin);
+        roles.add(basicUser);
+        user.setRoles(roles);
+
+        user.getRoles().remove(admin);
+
+        assert user.getRoles().size() == 1;
+    }
+}
