192ee4f2b1325db8feb007cafd0a6ef96cab11e9
kamilko250 <kamilko250@wp.pl>
2022-06-09 18:59:04 +0200

    first test

--- src/test/java/KKCH/StoreEverything/Information/InformationOrmTest.java
+++ src/test/java/KKCH/StoreEverything/Information/InformationOrmTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
-import java.time.Month;
 
 @SpringBootTest
 public class InformationOrmTest {
